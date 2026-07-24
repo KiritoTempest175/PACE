981856e8f16fe1cab4646680b77fb3c02184287d
zain-093 <24-cs-093@student.hitecuni.edu.pk>
2026-07-24 13:08:38 +0500

    little changes

--- masteries/api/router.py
+++ masteries/api/router.py
@@ -10,7 +10,7 @@ router = APIRouter()
 @router.get("/")
 def root():
     return {
-        "message": "PACE Backend Running 🚀"
+        "message": "PACE Backend Running"
     }
 
 @router.get("/health")
